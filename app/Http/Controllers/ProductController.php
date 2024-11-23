dda7100280b612b0cd47a559e627f0c6fea4e4a5
Omar Derbala <derbalajr@gmail.com>
2024-11-23 15:53:34 +0200

    Implement data loading and saving using JSON

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
+    private $fileName = 'products.json';
     public function index()
     {
         return view('products.index', ['products' => []]);
+    }
+
+    private function loadData()
+    {
+        if (!Storage::exists($this->fileName)) {
+            return [];
+        }
+
+        $content = Storage::get($this->fileName);
+        return json_decode($content, true) ?? [];
+    }
+
+    private function saveData($data)
+    {
+        Storage::put($this->fileName, json_encode($data, JSON_PRETTY_PRINT));
     }
 }
